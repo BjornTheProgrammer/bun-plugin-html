9c3223bdea41b2473d09ce91007e174be9d2c660
Bjorn <75190918+BjornTheProgrammer@users.noreply.github.com>
2024-11-22 19:16:01 -0700

    Fix tests to work with latest bun version

--- test/expected/preprocessor/main.js
+++ test/expected/preprocessor/main.js
@@ -1,7 +1,7 @@
-// /private/var/folders/xp/2h79jwp12nq0qc6hrr1lhlb00000gn/T/bun-build-9GQqGA/js/third.js
+// /private/var/folders/xp/2h79jwp12nq0qc6hrr1lhlb00000gn/T/bun-build-5ogXJz/js/third.js
 var third_default = { hello: "world" };
 
-// /var/folders/xp/2h79jwp12nq0qc6hrr1lhlb00000gn/T/bun-build-9GQqGA/main.ts
+// ../../../../../private/var/folders/xp/2h79jwp12nq0qc6hrr1lhlb00000gn/T/bun-build-5ogXJz/main.ts
 console.log(third_default);
 console.log("Running JS for browser");
 document.querySelector("#js-target").innerHTML = "Changed!";
