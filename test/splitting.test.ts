d2ad4b44828f0918f1eabd91bc75231777bb3170
Mindon Feng <mindonfeng@tencent.com>
2024-11-26 11:36:33 +0800

    feat: supports sass stylesheet and make isURL matching href="#.."

--- test/splitting.test.ts
+++ test/splitting.test.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import html from "../src/index";
-import { describe, expect } from "bun:test";
+import { expect, test, describe } from "bun:test";
 import { emptyDir } from "./utils";
 
 describe("Testing Splitting", async () => {
@@ -11,7 +11,11 @@ describe("Testing Splitting", async () => {
   await Bun.build({
     entrypoints: ["./test/splitting/index.html"],
     outdir: generationDirectory,
-    plugins: [html()],
+    plugins: [html({
+			naming: {
+				css: '[name]-[hash].[ext]',
+			}
+		})],
     root: ".",
     naming: {
       entry: "[dir]/[name].[ext]",
@@ -25,13 +29,26 @@ describe("Testing Splitting", async () => {
 
   const content = await Bun.file(entryHtml).text();
   const src: string[] = content.match(/[^"\/]+\.js/g) || [];
-  expect(src?.length).toBe(2);
-  expect(src[0].startsWith("x-")).toBeTrue();
-  expect(src[1].startsWith("y-")).toBeTrue();
-  expect(fs.existsSync(`${generationDirectory}/${src[0]}`));
-  expect(fs.existsSync(`${generationDirectory}/${src[1]}`));
-  const c = await Bun.file(`${generationDirectory}/${src[1]}`).text();
-  expect(c && c.indexOf(src[0]) > -1).toBeTrue();
-  const xc = await Bun.file(`${generationDirectory}/${src[0]}`).text();
-  expect(xc && xc.includes('https://cdn.jsdelivr.net/npm/luxon@3.5.0/+esm')).toBeTrue();
+  test(`Checking ts module compiled`, async () => {
+    expect(src?.length).toBe(2);
+    expect(src[0].startsWith("x-")).toBeTrue();
+    expect(src[1].startsWith("y-")).toBeTrue();
+    expect(fs.existsSync(`${generationDirectory}/${src[0]}`));
+    expect(fs.existsSync(`${generationDirectory}/${src[1]}`));
+  });
+  test(`Checking module splitting`, async () => {
+    const c = await Bun.file(`${generationDirectory}/${src[1]}`).text();
+    expect(c && c.indexOf(src[0]) > -1).toBeTrue();
+  });
+
+  test(`Checking import url`, async () => {
+    const xc = await Bun.file(`${generationDirectory}/${src[0]}`).text();
+    expect(xc && xc.includes('https://cdn.jsdelivr.net/npm/luxon@3.5.0/+esm')).toBeTrue();
+  });
+
+  test(`Checking .scss compiled`, async () => {
+    const m = content.match(/style-[^.]+.css/);
+    expect(m?.length).toBe(1);
+    expect(fs.existsSync(`${generationDirectory}/${m[0]}`));
+  });
 });
